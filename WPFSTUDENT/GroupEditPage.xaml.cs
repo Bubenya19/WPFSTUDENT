6009f6f4c204a7c40f98d2926014e63c897d9e7f
calc123 <123mail@mail.ru>
2022-09-28 09:37:39 +0500

    nada mnoga delat

--- WPFSTUDENT/GroupEditPage.xaml.cs
+++ WPFSTUDENT/GroupEditPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,8 +12,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace WPFSTUDENT
 {
@@ -20,9 +25,126 @@ namespace WPFSTUDENT
     /// </summary>
     public partial class GroupEditPage : Page
     {
+        private NpgsqlConnection connection;
+        public ObservableCollection<Group> Groups { get; set; }
+        public Group NewGroup { get; set; }
         public GroupEditPage()
         {
             InitializeComponent();
+
+
+            Groups = new ObservableCollection<Group>();
+
+            Binding binding = new Binding();
+            binding.Source = Groups;
+
+            StudList2.SetBinding(ListBox.ItemsSourceProperty, binding);
+
+            LoadGroup1();
+
+            DataContext = this;
+
+
         }
+
+        private void UpdateSpecEdit(object sender, RoutedEventArgs e)
+        {
+            Groups.Add(NewGroup);
+
+
+            int NumberGroup = Convert.ToInt32(NumberGr.Text.Trim());
+            int SpecGroup = Convert.ToInt32(SpecGr.Text.Trim());  
+            
+
+            string CourseGroup = CourseGr.Text.Trim();
+            if (SpecGroup == 0 && NumberGroup == 0 && CourseGroup.Length == 0) return;
+            
+
+
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = connection;
+            command.CommandText = "UPDATE \"group\" SET number = @a, course = @b, spec = @c";
+            command.Parameters.AddWithValue("@a", NpgsqlDbType.Integer, NumberGroup);
+            command.Parameters.AddWithValue("@b", NpgsqlDbType.Varchar, CourseGroup);
+            command.Parameters.AddWithValue("@c", NpgsqlDbType.Integer, SpecGroup);
+
+            int result = command.ExecuteNonQuery();
+            if (result == 1)
+            {
+                MessageBox.Show("Специальность успешно отредактирована!");
+                LoadSpec();
+            }
+        }
+
+        private void LoadSpec()
+        {
+
+
+
+            NpgsqlCommand command = new NpgsqlCommand();
+
+            command.Connection = connection;
+            command.CommandText = "SELECT code, namespec, qualification FROM speciality ORDER BY code";
+            NpgsqlDataReader result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Groups.Add(new Group(result.GetInt32(0), result.GetString(1), result.GetString(2)));
+                }
+            }
+            result.Close();
+
+        }
+
+        private void DeleteGroup(object sender, RoutedEventArgs e)
+        {
+            Groups.Add(NewGroup);
+
+
+            int NumberGroup = Convert.ToInt32(NumberGr.Text.Trim());
+
+
+
+            if (NumberGroup == 0) return;
+
+
+
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = connection;
+            command.CommandText = "DELETE FROM \"group\" WHERE number = @a ";
+            command.Parameters.AddWithValue("@a", NpgsqlDbType.Integer, NumberGroup);
+
+            int result = command.ExecuteNonQuery();
+            if (result == 1)
+            {
+                MessageBox.Show("Специальность удалена.");
+                LoadGroup1();
+            }
+        }
+
+        private void LoadGroup1()
+        {
+
+
+
+            NpgsqlCommand command = new NpgsqlCommand();
+
+            command.Connection = connection;
+            command.CommandText = "SELECT code, namespec, qualification FROM speciality ORDER BY code";
+            NpgsqlDataReader result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Groups.Add(new Group(result.GetInt32(0), result.GetString(1), result.GetString(2)));
+                }
+            }
+            result.Close();
+
+        }
+
+
+
     }
 }
--- WPFSTUDENT/StudentCreatePage.xaml.cs
+++ WPFSTUDENT/StudentCreatePage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Npgsql;
+using NpgsqlTypes;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,117 @@ namespace WPFSTUDENT
     /// </summary>
     public partial class StudentCreatePage : Page
     {
+        public ObservableCollection<Group> Groups { get; set; } = new ObservableCollection<Group>();
+        public Group NewGroup { get; set; }
+
+        
+        private NpgsqlConnection connection;
+        
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+        public Student NewStudent { get; set; }
+
         public StudentCreatePage()
         {
             InitializeComponent();
+
+            Connect("localhost", "5432", "Denis", "1234", "students");
+
+
+
+
+            Binding binding2 = new Binding();
+            binding2.Source = Students;
+            cmbGStudId.SetBinding(ComboBox.ItemsSourceProperty, binding2);
+            
+            LoadGroup();
+            LoadStud(); 
+
+            DataContext = this;
+        }
+
+        private void LoadGroup()
+        {
+            Groups.Clear();
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Select \"number\",\"course\", spec FROM \"group\" ORDER by \"number\"";
+            NpgsqlDataReader result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Groups.Add(new Group(result.GetInt32(0), result.GetString(1), result.GetString(2)));
+                }
+            }
+            result.Close();
+
+        }
+
+        private void CreateStudent(object sender, RoutedEventArgs e)
+        {
+
+
+
+            Students.Add(NewStudent);
+
+            int GroupStud = (cmbGStudId.SelectedItem as Group).Number1;
+
+            if (cmbGStudId.SelectedItem == null) return;
+
+
+            int Phone = Convert.ToInt32(textPhone.Text.Trim());
+            string Surname = textSurname.Text.Trim();
+            if (Phone == 0 && Surname.Length == 0) return;
+            
+            string Name = textName.Text.Trim();
+            if (Name.Length == 0) return;
+
+            string Patronymic = textPatronymic.Text.Trim();
+            if (Patronymic.Length == 0) return;
+
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = connection;
+            command.CommandText = "INSERT INTO \"student\"(phone, surname,  name, patronymic) VALUES(@a, @b, @c, @d, @e)";
+            command.Parameters.AddWithValue("@a", NpgsqlDbType.Integer, Phone);
+            command.Parameters.AddWithValue("@b", NpgsqlDbType.Varchar, Surname);
+            command.Parameters.AddWithValue("@c", NpgsqlDbType.Integer, GroupStud);
+            command.Parameters.AddWithValue("@d", NpgsqlDbType.Varchar, Name);
+            command.Parameters.AddWithValue("@e", NpgsqlDbType.Varchar, Patronymic);
+
+            int result = command.ExecuteNonQuery();
+            if (result == 1)
+            {
+                MessageBox.Show("Группа успешно добавлена!");
+                LoadGroup();
+            }
+            cmbGStudId.SelectedItem = null;
+        }
+
+        private void LoadStud()
+        {
+            Groups.Clear();
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT number, course, spec FROM \"group\" ORDER BY number";
+            NpgsqlDataReader result = command.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Groups.Add(new Group(result.GetInt32(0), result.GetString(1)));
+                }
+            }
+            result.Close();
+
+        }
+
+        private void Connect(string host, string port, string user, string pass, string dbname)
+        {
+            string cs = string.Format("Host=localhost;Username=postgres;Password=1234;Database=students");
+            NpgsqlConnection nc = new NpgsqlConnection(cs);
+            connection = new NpgsqlConnection(cs);
+            connection.Open();
+
         }
     }
 }
